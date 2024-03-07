d91d3a875551cbc887c469700faef3f795584b4a
mbenz12 <mbenz10120112@gmail.com>
2024-03-07 12:00:56 -0500

    vault contract completed

--- vault/src/lib.rs
+++ vault/src/lib.rs
@@ -1,20 +1,24 @@
 // Find all our documentation at https://docs.near.org
 use near_sdk::borsh::{BorshDeserialize, BorshSerialize};
-use near_sdk::{log, near_bindgen};
+use near_sdk::{env, NearToken, Gas, near_bindgen, AccountId, Promise, serde_json::json};
+
 
 // Define the contract structure
 #[near_bindgen]
 #[derive(BorshDeserialize, BorshSerialize)]
 #[borsh(crate = "near_sdk::borsh")]
 pub struct Contract {
-    greeting: String,
+    pub owner_contract: AccountId,
+    pub ft_contract:  Option<AccountId>,
+    pub amount: u128,
 }
 
-// Define the default, which automatically initializes the contract
 impl Default for Contract {
     fn default() -> Self {
         Self {
-            greeting: "Hello".to_string(),
+            owner_contract: env::predecessor_account_id(),
+            ft_contract: None, // You need to specify the default value for ft_contract
+            amount: 0, // You need to specify the default value for amount
         }
     }
 }
@@ -22,37 +26,56 @@ impl Default for Contract {
 // Implement the contract structure
 #[near_bindgen]
 impl Contract {
-    // Public method - returns the greeting saved, defaulting to DEFAULT_GREETING
-    pub fn get_greeting(&self) -> String {
-        self.greeting.clone()
+    #[init]
+    pub fn init(ft_contract: Option<AccountId>) -> Self {
+        Self {
+            owner_contract: env::predecessor_account_id(),
+            ft_contract,
+            amount: 0,
+        }
+    }
+    
+    pub fn deposit(
+        &mut self,
+        amount: u128
+    ) {
+        if let Some(ft_contract) = &self.ft_contract {
+            self.amount = amount;
+            Promise::new(ft_contract.clone()).function_call(
+                "ft_transfer".to_string(), 
+                json!({
+                    "receiver_id": env::current_account_id().to_string(),
+                    "amount": amount.to_string(),
+                    "msg": "",
+                }).to_string().into_bytes().to_vec(),
+                NearToken::from_yoctonear(1),
+                Gas::from_tgas(200),
+            );
+        } else {
+            let amount = env::attached_deposit();
+            self.amount = amount.as_yoctonear();
+            Promise::new(env::current_account_id()).transfer(amount);
+        }
     }
 
-    // Public method - accepts a greeting, such as "howdy", and records it
-    pub fn set_greeting(&mut self, greeting: String) {
-        log!("Saving greeting: {greeting}");
-        self.greeting = greeting;
-    }
-}
-
-/*
- * The rest of this file holds the inline tests for the code above
- * Learn more about Rust tests: https://doc.rust-lang.org/book/ch11-01-writing-tests.html
- */
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn get_default_greeting() {
-        let contract = Contract::default();
-        // this test did not call set_greeting so should return the default "Hello" greeting
-        assert_eq!(contract.get_greeting(), "Hello");
-    }
-
-    #[test]
-    fn set_then_get_greeting() {
-        let mut contract = Contract::default();
-        contract.set_greeting("howdy".to_string());
-        assert_eq!(contract.get_greeting(), "howdy");
+    pub fn withdraw(
+        &mut self,
+        owner: AccountId,
+    ) {
+        if let Some(ft_contract) = &self.ft_contract {
+            Promise::new(ft_contract.clone()).function_call(
+                "ft_transfer".to_string(), 
+                json!({
+                    "receiver_id": owner.to_string(),
+                    "amount": self.amount.to_string(),
+                    "msg": "",
+                }).to_string().into_bytes().to_vec(),
+                NearToken::from_yoctonear(1),
+                Gas::from_tgas(200),
+            );
+        } else {
+            Promise::new(owner).transfer(NearToken::from_yoctonear(self.amount));
+        }
+        self.amount = 0;
     }
 }
