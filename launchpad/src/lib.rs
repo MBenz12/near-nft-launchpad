c7f930af78107664e93bb9ff2a193819e3ad566a
mbenz12 <mbenz10120112@gmail.com>
2024-03-13 13:54:33 -0400

    launchpad launch event added

--- launchpad/src/lib.rs
+++ launchpad/src/lib.rs
@@ -2,13 +2,13 @@
 use near_sdk::borsh::{BorshDeserialize, BorshSerialize};
 use near_sdk::{
     near_bindgen, AccountId, env, Promise, NearToken, Gas,
-    serde_json::json,
+    serde_json::json, log
 };
 use near_sdk::json_types::U128;
-use near_sdk::collections::{LookupMap, UnorderedSet};
 use near_contract_standards::non_fungible_token::metadata::{
     NFTContractMetadata
 };
+use near_sdk::serde::Serialize;
 
 const NEAR_PER_STORAGE: u128 = 10_000_000_000_000_000_000;
 const NFT_CONTRACT_STORAGE: u128 = 20_000_000_000_000_000_000_000;
@@ -17,15 +17,11 @@ const NFT_CONTRACT_STORAGE: u128 = 20_000_000_000_000_000_000_000;
 #[near_bindgen]
 #[derive(BorshDeserialize, BorshSerialize)]
 #[borsh(crate = "near_sdk::borsh")]
-pub struct Contract {
-    pub collections_by_creator: LookupMap<AccountId, Vec<AccountId>>,
-}
+pub struct Contract {}
 
 impl Default for Contract {
     fn default() -> Self {
-        Self {
-            collections_by_creator: LookupMap::new(b"CollectionsByCreator".to_vec()),
-        }
+        Self {}
     }
 }
 
@@ -49,9 +45,6 @@ impl Contract {
 
         // Deploy the nft contract
         let nft_contract_id: AccountId = format!("{}-{}.{}", owner.to_string(), metadata.symbol.to_lowercase(), current_id).parse().unwrap();
-
-        let mut existing_collections = self.collections_by_creator.get(&owner).unwrap_or(vec![]);
-        existing_collections.push(&nft_contract_id);
 
         Promise::new(nft_contract_id.clone())
             .create_account()
@@ -78,8 +71,56 @@ impl Contract {
                 NearToken::from_yoctonear(0),
                 Gas::from_tgas(20)
             );
+        
+            Event::Launch {
+                creator_id: &owner,
+                mint_price: &mint_price,
+                mint_currency: mint_currency.as_ref(),
+                name: &metadata.name,
+                symbol: &metadata.symbol,
+            }
+            .emit();
     }
-    pub fn get_collections_by_creator(&self, creator_id: AccountId) -> Vec<AccountId> {
-        self.collections_by_creator.get(&creator_id).unwrap_or(vec![])
+}
+
+#[derive(Serialize, Debug, Clone)]
+#[serde(crate = "near_sdk::serde")]
+#[serde(tag = "event", content = "data")]
+#[serde(rename_all = "snake_case")]
+pub enum Event<'a> {
+    Launch {
+        creator_id: &'a AccountId,
+        mint_price: &'a U128,
+        name: &'a String,
+        symbol: &'a String,
+        #[serde(skip_serializing_if = "Option::is_none")]
+        mint_currency: Option<&'a AccountId>,
     }
+}
+
+impl Event<'_> {
+    pub fn emit(&self) {
+        emit_event(&self);
+    }
+}
+
+const EVENT_STANDARD: &str = "linear";
+const EVENT_STANDARD_VERSION: &str = "1.0.0";
+
+// Emit event that follows NEP-297 standard: https://nomicon.io/Standards/EventsFormat
+// Arguments
+// * `standard`: name of standard, e.g. nep171
+// * `version`: e.g. 1.0.0
+// * `event`: type of the event, e.g. nft_mint
+// * `data`: associate event data. Strictly typed for each set {standard, version, event} inside corresponding NEP
+pub(crate) fn emit_event<T: ?Sized + Serialize>(data: &T) {
+    let result = json!(data);
+    let event_json = json!({
+        "standard": EVENT_STANDARD,
+        "version": EVENT_STANDARD_VERSION,
+        "event": result["event"],
+        "data": [result["data"]]
+    })
+    .to_string();
+    log!(format!("EVENT_JSON:{}", event_json));
 }
