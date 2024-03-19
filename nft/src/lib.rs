8da8cc411af227ddbb8b1fdc6258ab0c181a31f0
mbenz12 <mbenz10120112@gmail.com>
2024-03-19 12:04:04 -0400

    removed unnecessary function

--- nft/src/lib.rs
+++ nft/src/lib.rs
@@ -21,7 +21,7 @@ use near_contract_standards::non_fungible_token::core::{
 };
 use near_contract_standards::non_fungible_token::enumeration::NonFungibleTokenEnumeration;
 use near_contract_standards::non_fungible_token::metadata::{
-    NFTContractMetadata, NonFungibleTokenMetadataProvider, TokenMetadata, NFT_METADATA_SPEC,
+    NFTContractMetadata, NonFungibleTokenMetadataProvider, TokenMetadata,
 };
 use near_contract_standards::non_fungible_token::NonFungibleToken;
 use near_contract_standards::non_fungible_token::events::NftMint;
@@ -64,7 +64,6 @@ pub struct Contract {
     pub ft_deposits: LookupMap<AccountId, Balance>,
 }
 
-const DATA_IMAGE_SVG_NEAR_ICON: &str = "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 288 288'%3E%3Cg id='l' data-name='l'%3E%3Cpath d='M187.58,79.81l-30.1,44.69a3.2,3.2,0,0,0,4.75,4.2L191.86,103a1.2,1.2,0,0,1,2,.91v80.46a1.2,1.2,0,0,1-2.12.77L102.18,77.93A15.35,15.35,0,0,0,90.47,72.5H87.34A15.34,15.34,0,0,0,72,87.84V201.16A15.34,15.34,0,0,0,87.34,216.5h0a15.35,15.35,0,0,0,13.08-7.31l30.1-44.69a3.2,3.2,0,0,0-4.75-4.2L96.14,186a1.2,1.2,0,0,1-2-.91V104.61a1.2,1.2,0,0,1,2.12-.77l89.55,107.23a15.35,15.35,0,0,0,11.71,5.43h3.13A15.34,15.34,0,0,0,216,201.16V87.84A15.34,15.34,0,0,0,200.66,72.5h0A15.35,15.35,0,0,0,187.58,79.81Z'/%3E%3C/g%3E%3C/svg%3E";
 const NEAR_PER_STORAGE: u128 = 10_000_000_000_000_000_000;
 //the minimum storage to have a sale on the contract.
 const STORAGE_PER_SALE: u128 = 1000 * NEAR_PER_STORAGE;
@@ -86,25 +85,25 @@ enum StorageKey {
 impl Contract {
     /// Initializes the contract owned by `owner_id` with
     /// default metadata (for example purposes only).
-    #[init]
-    pub fn new_default_meta(owner_id: AccountId) -> Self {
-        Self::new(
-            owner_id,
-            NFTContractMetadata {
-                spec: NFT_METADATA_SPEC.to_string(),
-                name: "Example NEAR non-fungible token".to_string(),
-                symbol: "EXAMPLE".to_string(),
-                icon: Some(DATA_IMAGE_SVG_NEAR_ICON.to_string()),
-                base_uri: None,
-                reference: None,
-                reference_hash: None,
-            },
-            U128(0),
-            None,
-            U128(0),
-            U128(0),
-        )
-    }
+    // #[init]
+    // pub fn new_default_meta(owner_id: AccountId) -> Self {
+    //     Self::new(
+    //         owner_id,
+    //         NFTContractMetadata {
+    //             spec: NFT_METADATA_SPEC.to_string(),
+    //             name: "Example NEAR non-fungible token".to_string(),
+    //             symbol: "EXAMPLE".to_string(),
+    //             icon: Some(DATA_IMAGE_SVG_NEAR_ICON.to_string()),
+    //             base_uri: None,
+    //             reference: None,
+    //             reference_hash: None,
+    //         },
+    //         U128(0),
+    //         None,
+    //         U128(0),
+    //         U128(0),
+    //     )
+    // }
 
     #[init]
     pub fn new(
